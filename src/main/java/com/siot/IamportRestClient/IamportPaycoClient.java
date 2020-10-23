be668f1570cc7786d3eb6168e197f3ed2c830613
jang <jang@siot.do>
2020-10-24 07:34:34 +0900

    IamportClient 상속받은 IamportPaycoClient 에도 static ip 적용

--- src/main/java/com/siot/IamportRestClient/IamportPaycoClient.java
+++ src/main/java/com/siot/IamportRestClient/IamportPaycoClient.java
@@ -19,31 +19,36 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class IamportPaycoClient extends IamportClient {
 
 	private PaycoImpl paycoImpl;
-	
+
 	public IamportPaycoClient(String api_key, String api_secret) {
 		super(api_key, api_secret);
-		this.paycoImpl = this.createImpl();
+		this.paycoImpl = this.createImpl(false);
 	}
-	
-	private PaycoImpl createImpl() {
+
+	public IamportPaycoClient(String api_key, String api_secret, boolean useStaticIP) {
+		super(api_key, api_secret, useStaticIP);
+		this.paycoImpl = this.createImpl(useStaticIP);
+	}
+
+	private PaycoImpl createImpl(boolean useStaticIP) {
 		OkHttpClient client = new OkHttpClient.Builder()
 									.readTimeout(30, TimeUnit.SECONDS)
 									.connectTimeout(10, TimeUnit.SECONDS)
 									.build();
-		
+
 		Retrofit retrofit = new Retrofit.Builder()
-								.baseUrl(API_URL)
+								.baseUrl(useStaticIP ? STATIC_API_URL:API_URL)
 								.addConverterFactory(GsonConverterFactory.create())
 								.client(client)
 								.build();
-		
+
 		return retrofit.create(PaycoImpl.class);
 	}
-	
+
 	public IamportResponse<OrderStatus> updateOrderStatus(String impUid, String status) throws IamportResponseException, IOException {
 		AccessToken auth = getAuth().getResponse();
 		Call<IamportResponse<OrderStatus>> call = this.paycoImpl.updateStatus(auth.getToken(), impUid, new OrderStatusData(status));
-		
+
 		Response<IamportResponse<OrderStatus>> response = call.execute();
 		if ( !response.isSuccessful() )	throw new IamportResponseException( getExceptionMessage(response), new HttpException(response) );
 
