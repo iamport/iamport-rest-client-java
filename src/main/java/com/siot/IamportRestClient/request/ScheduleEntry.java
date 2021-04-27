2d28ea60f9619f35da88f511c336434cd1d5c973
Hyukgyu Kwon <me@kwon.hk>
2021-04-27 14:07:51 +0900

    API 문서에만 존재하던 schedule API의 선택항목 옵션 추가

--- src/main/java/com/siot/IamportRestClient/request/ScheduleEntry.java
+++ src/main/java/com/siot/IamportRestClient/request/ScheduleEntry.java
@@ -7,31 +7,115 @@ import com.google.gson.annotations.SerializedName;
 
 public class ScheduleEntry {
 
-	@SerializedName("merchant_uid")
-	private String merchant_uid;
-	
-	@SerializedName("schedule_at")
-	private Date schedule_at;
-	
-	@SerializedName("amount")
-	private BigDecimal amount;
+    @SerializedName("merchant_uid")
+    private String merchant_uid;
 
-	public ScheduleEntry(String merchant_uid, Date schedule_at, BigDecimal amount) {
-		this.merchant_uid = merchant_uid;
-		this.schedule_at = schedule_at;
-		this.amount = amount;
-	}
+    @SerializedName("schedule_at")
+    private Date schedule_at;
 
-	public String getMerchantUid() {
-		return merchant_uid;
-	}
+    @SerializedName("amount")
+    private BigDecimal amount;
 
-	public Date getScheduleAt() {
-		return schedule_at;
-	}
+    @SerializedName("tax_free")
+    private BigDecimal tax_free;
 
-	public BigDecimal getAmount() {
-		return amount;
-	}
-	
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("buyer_name")
+    private String buyer_name;
+
+    @SerializedName("buyer_email")
+    private String buyer_email;
+
+    @SerializedName("buyer_tel")
+    private String buyer_tel;
+
+    @SerializedName("buyer_addr")
+    private String buyer_addr;
+
+    @SerializedName("buyer_postcode")
+    private String buyer_postcode;
+
+    @SerializedName("custom_data")
+    private String custom_data;
+
+    @SerializedName("notice_url")
+    private String notice_url;
+
+    public ScheduleEntry(String merchant_uid, Date schedule_at, BigDecimal amount) {
+        this.merchant_uid = merchant_uid;
+        this.schedule_at = schedule_at;
+        this.amount = amount;
+    }
+
+    public void setOptionalValues(
+            BigDecimal tax_free,
+            String name,
+            String buyer_name,
+            String buyer_email,
+            String buyer_tel,
+            String buyer_addr,
+            String buyer_postcode,
+            String custom_data,
+            String notice_url
+    ) {
+        this.tax_free = tax_free;
+        this.name = name;
+        this.buyer_name = buyer_name;
+        this.buyer_email = buyer_email;
+        this.buyer_tel = buyer_tel;
+        this.buyer_addr = buyer_addr;
+        this.buyer_postcode = buyer_postcode;
+        this.custom_data = custom_data;
+        this.notice_url = notice_url;
+    }
+
+    public String getMerchantUid() {
+        return merchant_uid;
+    }
+
+    public Date getScheduleAt() {
+        return schedule_at;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public BigDecimal getTaxFree() {
+        return tax_free;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBuyerName() {
+        return buyer_name;
+    }
+
+    public String getBuyerEmail() {
+        return buyer_email;
+    }
+
+    public String getBuyerTel() {
+        return buyer_tel;
+    }
+
+    public String getBuyerAddr() {
+        return buyer_addr;
+    }
+
+    public String getBuyerPostcode() {
+        return buyer_postcode;
+    }
+
+    public String getCustomData() {
+        return custom_data;
+    }
+
+    public String getNoticeUrl() {
+        return notice_url;
+    }
 }
