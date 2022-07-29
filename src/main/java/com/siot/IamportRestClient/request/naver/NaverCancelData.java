8b821e9d143003619b4538fc0a8543cf5a90367c
Lens0021 / Leslie <lorentz0021@gmail.com>
2022-07-29 11:16:28 +0900

    feat: 네이버 환불 코드 추가

--- src/main/java/com/siot/IamportRestClient/request/naver/NaverCancelData.java
+++ src/main/java/com/siot/IamportRestClient/request/naver/NaverCancelData.java
@@ -4,9 +4,13 @@ import com.google.gson.annotations.SerializedName;
 
 public class NaverCancelData {
 
-	public static final String REASON_PRODUCT_UNSATISFIED = "PRODUCT_UNSATISFIED";
-	public static final String REASON_DELAYED_DELIVERY    = "DELAYED_DELIVERY";
 	public static final String REASON_SOLD_OUT            = "SOLD_OUT";
+	public static final String REASON_DELAYED_DELIVERY    = "DELAYED_DELIVERY";
+	public static final String REASON_PRODUCT_UNSATISFIED = "PRODUCT_UNSATISFIED";
+	public static final String REASON_INTENT_CHANGED      = "INTENT_CHANGED";
+	public static final String REASON_COLOR_AND_SIZE      = "COLOR_AND_SIZE";
+	public static final String REASON_WRONG_ORDER         = "WRONG_ORDER";
+	public static final String REASON_INCORRECT_INFO      = "INCORRECT_INFO";
 	
 	@SerializedName("product_order_id")
 	private String[] product_order_id;
@@ -25,9 +29,13 @@ public class NaverCancelData {
 	}
 	
 	private boolean isValidReason(String reason) {
-		return	REASON_PRODUCT_UNSATISFIED.equals(reason) ||
+		return	REASON_SOLD_OUT.equals(reason)            ||
 				REASON_DELAYED_DELIVERY.equals(reason)    ||
-				REASON_SOLD_OUT.equals(reason);
+				REASON_PRODUCT_UNSATISFIED.equals(reason) ||
+				REASON_INTENT_CHANGED.equals(reason)      ||
+				REASON_COLOR_AND_SIZE.equals(reason)      ||
+				REASON_WRONG_ORDER.equals(reason)         ||
+				REASON_INCORRECT_INFO.equals(reason);
 	}
 	
 }
